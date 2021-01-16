10730d083636beacb63212af4761ce32aa5ad810
Somnath Sardar <githubsomnath1996@gmail.com>
2021-01-17 01:03:09 +0530

    index.html, main.js and style.css modified

--- main.js
+++ main.js
@@ -1,8 +1,42 @@
+window.onload = function(e){
+    let loader = document.getElementById("loader");
+    setTimeout(()=>{
+        stopAnimation(loader)
+        .then(()=>{
+            setTimeout(()=>{
+                loader.remove();
+            }, 1000);
+        });
+    }, 1000);
+}
+
+function stopAnimation(e){
+    return new Promise((resolve, reject) =>{
+        e.style.transition = '0.8s';
+        e.style.opacity = 0;
+        resolve(true);
+    });
+}
+
 function toggleClass(el){
-    if(el.className == 'class1'){
-        el.className='class2';
+    let className = el.className;
+    if(className.includes('off-state')){
+        el.classList.remove('off-state');
+        el.classList.add('on-state');
     }
     else{
-        el.className='class1';
+        el.classList.remove('on-state');
+        el.classList.add('off-state');
+    }
+
+    el = el.querySelector('.circle')
+    className = el.className;
+    if(className.includes('circle-green')){
+        el.classList.remove('circle-green');
+        el.classList.add('circle-gray');
+    }
+    else{
+        el.classList.remove('circle-gray');
+        el.classList.add('circle-green');
     }
 }
